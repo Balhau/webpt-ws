bc2747b2bb9071ded413ba0734aca16fb2526907
Vitor Fernandes <vitor.fernandes@blip.pt>
2015-07-10 15:46:30 +0100

    Added base controller

---
+++ src/main/java/org/pt/pub/data/ws/base/BaseController.java
@@ -0,0 +1,44 @@
+package org.pt.pub.data.ws.base;
+
+import java.util.List;
+
+import org.pt.pub.data.sources.base.Base;
+import org.pt.pub.data.sources.base.domain.BaseEntry;
+import org.pt.pub.data.sources.base.domain.BaseQueryResponse;
+import org.pt.pub.data.sources.rbe.Rbe;
+import org.pt.pub.data.sources.rbe.domain.RbeIndicator;
+import org.pt.pub.data.sources.rbe.domain.RbeIndicatorData;
+import org.pt.pub.data.ws.domain.WebResult;
+import org.pt.pub.global.domain.TableData;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/ws/base")
+public class BaseController {
+	private Base base;
+	
+	public BaseController() {
+		base=new Base();
+	}
+	
+	@RequestMapping("/contracts/{start}/{end}")
+	public WebResult<BaseQueryResponse> getContracts(
+			@PathVariable("start") int startOffset,
+			@PathVariable("end") int endOffset
+			){
+		return WebResult.<BaseQueryResponse>wrap(()->{
+			return base.getAllResults(startOffset, endOffset);
+		});
+	}
+	
+	@RequestMapping("/contract/{idContract}")
+	public WebResult<List<TableData>> getIndicatorData(
+			@PathVariable("idContract") int idContract){
+		return WebResult.<List<TableData>>wrap(()->{
+			return base.getEntryInformationByContractoId(idContract);
+		});
+	}
+}
