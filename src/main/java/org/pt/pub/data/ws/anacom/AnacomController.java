802eef759dcc13c364076c1e6fefd302f83725a3
Vitor Fernandes <vitor.fernandes@blip.pt>
2015-10-24 10:55:08 +0100

    Added television services endpoint for the anacom provider

--- src/main/java/org/pt/pub/data/ws/anacom/AnacomController.java
+++ src/main/java/org/pt/pub/data/ws/anacom/AnacomController.java
@@ -4,7 +4,8 @@ package org.pt.pub.data.ws.anacom;
 import java.util.List;
 
 import org.pt.pub.data.sources.anacom.Anacom;
-import org.pt.pub.data.sources.anacom.domain.Tarifa;
+import org.pt.pub.data.sources.anacom.domain.TarifaInternet;
+import org.pt.pub.data.sources.anacom.domain.TarifaTelevisao;
 import org.pt.pub.data.ws.domain.WebResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,17 @@ public class AnacomController {
         this.anacom=new Anacom();
     }
 
-    @RequestMapping("/tarifarios")
-    public WebResult<List<Tarifa>> getTarifarios(){
-        return WebResult.<List<Tarifa>>wrap(
-                ()->anacom.getTarifarios()
+    @RequestMapping("/tarifarios/internet")
+    public WebResult<List<TarifaInternet>> getInternetTarifarios(){
+        return WebResult.<List<TarifaInternet>>wrap(
+                ()->anacom.getTarifariosInternet()
+        );
+    }
+
+    @RequestMapping("/tarifarios/televisao")
+    public WebResult<List<TarifaTelevisao>> getTelevisaoTarifarios(){
+        return WebResult.<List<TarifaTelevisao>>wrap(
+                () -> anacom.getTarifariosTelevisao()
         );
     }
 }
