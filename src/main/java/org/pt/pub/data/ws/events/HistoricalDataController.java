828a243110b6c5dd22113299e6f470c93459fbf4
Vitor Fernandes <vitor.fernandes@blip.pt>
2015-11-07 19:36:16 +0000

    Added more controllers for the historical data

--- src/main/java/org/pt/pub/data/ws/events/HistoricalDataController.java
+++ src/main/java/org/pt/pub/data/ws/events/HistoricalDataController.java
@@ -3,6 +3,8 @@ package org.pt.pub.data.ws.events;
 import org.pt.pub.data.sources.events.HistoricalEvents;
 import org.pt.pub.data.sources.events.domain.HistoricalEvent;
 import org.pt.pub.data.ws.domain.WebResult;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +24,42 @@ public class HistoricalDataController {
 
     @RequestMapping("/today")
     public WebResult<List<HistoricalEvent>> getTodayEvents(){
-        return WebResult.wrap(() -> { return historicalEvents.getTodayEvents();});
+        return WebResult.wrap(() -> {
+            return historicalEvents.getTodayEvents();
+        });
+    }
+
+    @RequestMapping("/event/{month}/{day}")
+    @Cacheable("cache")
+    public WebResult<List<HistoricalEvent>> getEventsByMonthDay(
+        @PathVariable("month") int month,
+        @PathVariable("day") int day){
+
+        return WebResult.wrap(() -> {
+            return historicalEvents.getEventsByDay(month,day);
+        });
+    }
+
+    @RequestMapping("/births/{month}/{day}")
+    @Cacheable("cache")
+    public WebResult<List<HistoricalEvent>> getBirthsByMonthDay(
+            @PathVariable("month") int month,
+            @PathVariable("day") int day){
+
+        return WebResult.wrap(() -> {
+            return historicalEvents.getBirthdaysByDay(month,day);
+        });
+    }
+
+    @RequestMapping("/deaths/{month}/{day}")
+    @Cacheable("cache")
+    public WebResult<List<HistoricalEvent>> getDeathsByMonthDay(
+            @PathVariable("month") int month,
+            @PathVariable("day") int day){
+
+        return WebResult.wrap(() -> {
+            return historicalEvents.getDeathsByDay(month,day);
+        });
     }
 
 }
